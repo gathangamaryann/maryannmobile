c018dc08d6505494e08ebb1ec6880383bd143af5
gathangamaryann <maryanngathanga@gmail.com>
2022-02-12 17:26:36 +0300

    first commit

---
+++ Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var name = "Maryann Gathanga" // Int
+    println(name)
+    var weight = 54 // Int
+    println(weight)
+    var myNum = 254787523631L // Long
+    println(myNum)
+    var age = 21
+    println(age)
+    var Kenyan: Boolean = true
+    var notKenyan: Boolean = false
+    println(Kenyan)
+
+
+}
